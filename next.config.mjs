fcf9e4ceae56770ca5a41c3295f400b7cef4b564
Yuto Shima <yutoshima1234@gmail.com>
2024-12-23 07:59:41 +0900

    GitHubPagesの設定

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,15 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    experimental: {
+        mdxRs: true,
+    },
 };
 
 export default withContentlayer(nextConfig);
+
+module.exports = {
+  basePath: process.env.GITHUB_ACTIONS && "/my-profile-page",
+  trailingSlash: true,
+};
